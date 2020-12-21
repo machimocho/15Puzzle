f5f3adede8cbf04d009511a0a653eafa23ffa105
Mario Antonio Alvarez Gómez <machimocho@gmail.com>
2020-12-21 16:46:27 -0600

    Logic concluded

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 // State
-let empthyIndex
+let boxLength = 4;
+let empthyIndex;
+let solvedMessage = document.getElementById("win");
+let box = document.getElementById("box");
+let solvedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ''];
 
 // Functions
 const drawInitialNumbers = () => {
-  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""];
+  let numbers = [...solvedNumbers]
   let squares = document.getElementsByClassName("square");
 
+  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   function shuffle(array) {
     var currentIndex = array.length,
       temporaryValue,
@@ -30,13 +35,69 @@ const drawInitialNumbers = () => {
 
   for (let index = 0; index < numbers.length; index++) {
     const element = numbers[index];
-    squares[index].innerHTML = element;
+    squares[index].innerText = element;
     if (element == "") empthyIndex = index;
   }
-}
+};
 
 const startGame = () => {
+  solvedMessage.style.display = "none";
+  box.addEventListener("click", handleclick);
   drawInitialNumbers();
+};
+
+const verifySolved = () => {
+    let squares = Array.from(document.getElementsByClassName("square")).map(function(ele){
+        return isNaN(ele.innerText == '' ? 'x' : ele.innerText) ? '' : parseInt(ele.innerText, 10)
+    });
+    let ordered = (JSON.stringify(solvedNumbers) === JSON.stringify(squares));
+    if (ordered){
+        solvedMessage.style.display = "";
+        box.removeEventListener("click", handleclick);
+    }
 }
+
+const toogle = (divClicked) => {
+  let clickedIndex = parseInt(divClicked.id.substr(2), 10);
+  let divEmpthy = document.getElementById(`sq${empthyIndex}`);
+
+  divEmpthy.innerText = divClicked.innerText;
+  divClicked.innerText = "";
+  empthyIndex = clickedIndex;
+};
+
+const applyChanges = (sqr) => {
+    toogle(sqr);
+    verifySolved()
+}
+
+const handleclick = (e) => {
+  let sqr = e.target;
+  if (sqr.innerText != "") {
+    let clickedIndex = parseInt(sqr.id.substr(2), 10);
+    // Verify if the clicked square is next to the empthy one
+    if (clickedIndex + boxLength == empthyIndex) {
+      // Up
+     applyChanges(sqr)
+    } else if (clickedIndex - boxLength == empthyIndex) {
+      // Down
+     applyChanges(sqr)
+    } else if (
+      clickedIndex - 1 == empthyIndex &&
+      clickedIndex % boxLength != 0
+    ) {
+      // Left
+     applyChanges(sqr)
+    } else if (
+      clickedIndex + 1 == empthyIndex &&
+      clickedIndex % boxLength != boxLength - 1
+    ) {
+      // Right
+     applyChanges(sqr)
+    }
+  }
+};
+
+
 
 startGame();
